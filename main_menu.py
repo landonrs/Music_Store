77b7e2bc0b9e15d5802dd6fa96d94b4d5ff8d5d8
landonrs <altruist8@yahoo.com>
2017-11-15 17:49:26 -0700

    added button and edit cascade

--- main_menu.py
+++ main_menu.py
@@ -3,22 +3,29 @@ from tkinter import *
 def callback():
     print("called the callback!")
 
-root = Tk()
+tk_library = Tk()
 
 # create a menu
-menu = Menu(root)
-root.config(menu=menu)
+main_menu = Menu(tk_library)
+tk_library.config(menu=main_menu)
 
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
+filemenu = Menu(main_menu)
+main_menu.add_cascade(label="File", menu=filemenu)
 filemenu.add_command(label="New", command=callback)
 filemenu.add_command(label="Open...", command=callback)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=callback)
 
-helpmenu = Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu = Menu(main_menu)
+main_menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About...", command=callback)
+
+edit = Menu(main_menu)
+main_menu.add_cascade(label="Edit", menu=edit)
+edit.add_command(label="undo", command=callback)
+
+b = Button(tk_library, text="OK", command=callback)
+b.pack()
 
 mainloop()
 
