882b9f85468574deb81ca59a25203fa9894effbd
landonrs <altruist8@yahoo.com>
2017-11-18 19:27:33 -0700

    added frame classes to main_manager file

--- main_manager.py
+++ main_manager.py
@@ -3,8 +3,12 @@ from inventory_editor import Inv_Editor
 from log_in_controller import Login_Controller
 from searcher import Searcher
 from sorter import Sorter
-from tkinter import *
-from main_menu import Main_Menu
+import tkinter as tk
+from tkinter import font as tkfont
+from tkinter import Entry
+from tkinter import Button
+from tkinter import messagebox
+from tkinter import END
 import sqlite3
 
 
@@ -18,14 +22,13 @@ class Main_Manager(Agent):
         self.login_controller = Login_Controller()
         self.searcher = Searcher()
         self.sorter = Sorter()
-        self.login_window = Tk()
-        self.create_main_window()
-        self.main_menu = None
+        self.username_entry = None
+        self.password_entry = None
+        self.main_menu = Main_Menu()
         self.working = True
         self.username = ""
         self.password = ""
         self.initialize_DB()
-        #self.main_loop = self.login_window.mainloop()
 
     def initialize_DB(self):
         conn = sqlite3.connect('music_store.db')
@@ -47,27 +50,9 @@ class Main_Manager(Agent):
         conn.commit()
         conn.close()
 
-    def create_main_window(self):
-        self.login_window.minsize(width=300, height=300)
-        # create a menu
-        main_menu = Menu(self.login_window)
-        self.login_window.config(menu=main_menu)
-        username_entry = Entry(self.login_window)
-        username_entry.insert(0, "Username")
-        username_entry.place(x=100,y=100)
-        password_entry = Entry(self.login_window, show="*")
-        password_entry.insert(0, "Password")
-        password_entry.place(x=100, y=125)
-        username_entry.focus_set()
-        login_button = Button(self.login_window, text="Login", command= lambda:self.login(username_entry.get(), password_entry.get()))
-        login_button.config(width=15)
-        login_button.place(x=100, y=150)
-
     def open_main_menu(self):
         print("opening menu")
-        self.main_menu = Main_Menu()
-        self.login_window.destroy()
-        #self.main_loop = self.main_menu.mainloop()
+        self.main_menu.show_frame("StartPage")
 
     def login(self, username, password):
         self.username = username
@@ -76,9 +61,110 @@ class Main_Manager(Agent):
         valid_login = self.ask("login_controller", "valid_login")
         if valid_login:
             self.open_main_menu()
+        else:
+            self.main_menu.display_login_error()
+
+class Main_Menu(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.minsize(height="300", width="500")
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (StartPage, SearchPage, InsertPage, LoginPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    def display_login_error(self):
+        messagebox.showerror("Incorrect login", "Username or password is incorrect, please try again.")
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label.pack(side="top", fill="x")
+
+        button1 = tk.Button(self, text="Go to Search Page",
+                            command=lambda: controller.show_frame("SearchPage"))
+        button2 = tk.Button(self, text="Add items to Inventory",
+                            command=lambda: controller.show_frame("InsertPage"))
+        button1.pack()
+        button2.pack()
+
+
+class SearchPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the Search page", font=controller.title_font)
+        label.pack(side="top", fill="x")
+
+        button1 = tk.Button(self, text="return to main menu",
+                            command=lambda: controller.show_frame("StartPage"))
+
+        button1.pack()
+
+
+class InsertPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the Insert page", font=controller.title_font)
+        label.pack(side="top", fill="x")
+        item_name = Entry(self)
+        item_name.place(x=100, y=100)
+        name = tk.Label(self, text="name:")
+        name.place(x=70, y=100)
+
+        button1 = tk.Button(self, text="return to main menu",
+                            command=lambda: controller.show_frame("StartPage"))
+        button1.place(x=150, y=200)
+
+
+class LoginPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Employee Login", font=controller.title_font)
+        label.pack(side="top", fill="x")
+        username_entry = Entry(self)
+        username_entry.insert(0, "landonrs")
+        username_entry.place(x=100, y=100)
+        password_entry = Entry(self, show="*")
+        password_entry.insert(0, "music8")
+        password_entry.place(x=100, y=125)
+        username_entry.focus_set()
+        login_button = Button(self, text="Login",
+                              command=lambda: boss.login(username_entry.get(), password_entry.get()))
+        login_button.config(width=15)
+        login_button.place(x=100, y=150)
+
+
 
 boss = Main_Manager(None)
-mainloop()
-
-
-
+boss.main_menu.mainloop()
